35e3585f6b218a42ee6e6a8d70da02bfeb7b32e0
yevhenii <yevheniidomahaiev@gmail.con>
2024-09-24 05:39:00 -0500

    bankApp1

--- src/main/java/org/domahaiev/bankapp/config/SecurityConfig.java
+++ src/main/java/org/domahaiev/bankapp/config/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .defaultSuccessUrl("/home", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
